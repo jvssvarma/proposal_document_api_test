57a02b0aa0ae8b646ba080d1892f4af986517f1a
jvssvarma <varma.santosh@outlook.com>
2016-12-16 06:11:13 -0800

    Proposals scaffold added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-6.times  do |x|
-  TemplateDocument.create!(title: "Doc #{x}", body: "This a mock test for body of the template document",
+6.times  do |doc_file|
+  TemplateDocument.create!(title: "Doc #{doc_file}", body: "This a mock test for body of the template document",
                            file_url: "https://g.twimg.com/about/products/tweetdeck/modal/modal1.jpg",
                            image_url: "https://about.twitter.com/products/tweetdeck")
+end
+
+6.times do |proposal|
+	Proposal.create!(
+		customer: "Customer #{proposal}",
+		portfolio_url: 'http://codevarma.com',
+		tools: 'Ruby on Rails, Angular 2, and Postgres',
+		estimated_hours: (80 + proposal),
+		hourly_rate: 120,
+		weeks_to_complete: 12,
+		client_email: 'jordan@devcamp.com',
+	)
 end
